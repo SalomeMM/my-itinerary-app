b7c63c401a689c31cbd2504c67372afbe21cc9ee
SalomeMM <sal.mm3@gmail.com>
2020-04-02 10:56:42 +0200

    new repo

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Server is running on " + port + "port");
+});
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(cors());
+
+// mongoose
+//   .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+//   .then(() => console.log("Connection to Mongo DB established"))
+//   .catch(err => console.log(err));
