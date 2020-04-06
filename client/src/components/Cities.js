a4ab74e17f3fb6c9ae108ce22b9234eb587641d8
SalomeMM <sal.mm3@gmail.com>
2020-04-06 19:40:59 +0200

    client manually updated

---
+++ client/src/components/Cities.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+// export default function Cities() {
+
+class Cities extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     books: []
+  //   };
+  // }
+
+  componentDidMount() {
+    var requestOptions = {
+      method: "GET",
+      // redirect: 'follow'
+    };
+
+    fetch("http://localhost:5000/cities/all", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+
+    return (
+      <div>
+        <h1>Cities</h1>
+      </div>
+    );
+  }
+}
+
+export default Cities;
+
+// export default function Cities() {
+
+//   componentDidMount() {
+//     var requestOptions = {
+//       method: 'GET',
+//       // redirect: 'follow'
+//     };
+
+//     fetch("http://localhost:5000/cities/all", requestOptions)
+//       .then(response => response.text())
+//       .then(result => console.log(result))
+//       .catch(error => console.log('error', error));
+
+//   return (
+//     <div>
+//       <h1>Cities</h1>
+//     </div>
+//   );
+// }
+//end
