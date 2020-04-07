7905664140b66a2eda70867ca5ea3e1bc7bde90d
SalomeMM <sal.mm3@gmail.com>
2020-04-07 16:24:41 +0200

    client working

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./store/reducers/rootReducer";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
